6aea7af8dca7c6f31a10da0ce15576c9db983f8b
rakibsarowar <rakibsarowar@gmail.com>
2023-04-18 21:53:05 +0600

    added moment js

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import logo from '../../../assets/logo.png'
+import moment from 'moment';
 
 const Header = () => {
     return (
         <div>
-            <h1>Header</h1>
+            <div className="text-center">
+            <img src={logo} alt="" />
+            <p className='text-secondary'><small>Journalism Without Fear or Fabour</small></p>
+            <p>{moment().format("dddd, MMMM D, YYYY")}</p>
+            </div>
         </div>
     );
 };
