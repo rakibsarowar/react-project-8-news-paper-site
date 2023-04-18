2295daaf8558f0dd373665f3e468a016f5b73a50
rakibsarowar <rakibsarowar@gmail.com>
2023-04-18 20:17:50 +0600

    step 3.1 import css in main jsx

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqWf_1gP3_sdfzrwAmQ2c3QALz68qy3RQ",
+  authDomain: "news-site-adbe2.firebaseapp.com",
+  projectId: "news-site-adbe2",
+  storageBucket: "news-site-adbe2.appspot.com",
+  messagingSenderId: "473777801716",
+  appId: "1:473777801716:web:f150edbf4223915cf911a1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
