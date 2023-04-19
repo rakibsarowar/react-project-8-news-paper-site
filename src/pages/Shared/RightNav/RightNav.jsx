c9d89c0fbf3bcbea92d4a46d0112b36d47125240
rakibsarowar <rakibsarowar@gmail.com>
2023-04-19 17:01:22 +0600

    Step 8_ added List Group

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 const RightNav = () => {
     return (
         <div>
-            <h2>Right Nav</h2>
+            <h4>Login With</h4>
+            <Button className='mb-2' variant="primary"><FaGoogle />Login with Google</Button>
+            <Button variant="secondary"><FaGithub />Login with Github</Button>
+            <div>
+                <h4>Find us on</h4>
+                <ListGroup>
+                    <ListGroup.Item><FaFacebook/> Facebook</ListGroup.Item>
+                    <ListGroup.Item><FaTwitter/> Twitter</ListGroup.Item>
+                    <ListGroup.Item><FaInstagram/> Instagram</ListGroup.Item>
+                </ListGroup>
+            </div>
         </div>
     );
 };
