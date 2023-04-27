8230a7c0252c9fdfcee15cc3698304c37cbd8756
rakibsarowar <rakibsarowar@gmail.com>
2023-04-27 15:48:25 +0600

    making private routing

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -3,16 +3,17 @@ import React, { createContext } from 'react';
 // Step 01-----------------------------------
 export const authContext = createContext(null);
 
-const AuthProvider = () => {
+const AuthProvider = (children) => {
 
     // Step 02 -----------------------------
     const authInfo = {
         
     };
 
+    // Step 03 ------------------------------- add children
     return (
         <authContext.Provider value={authInfo}>
-            
+            {children}
         </authContext.Provider>
     );
 };
