f74dfcbad9814614de5a9f9ee2c5d1672dcb2f31
rakibsarowar <rakibsarowar@gmail.com>
2023-04-27 15:35:22 +0600

    added card in below of blog

--- src/pages/News/News/News.jsx
+++ src/pages/News/News/News.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
 import { Link, useLoaderData } from 'react-router-dom';
+import EditorsNews from '../EditorsNews/EditorsNews';
 
 const News = () => {
     const news = useLoaderData();
-    const { _id, title, details, image_url,author,category_id } = news;
+    const { _id, title, details, image_url, author, category_id } = news;
 
     return (
-        <Card>
-            <Card.Img variant="top" src={image_url} />
-            <Card.Body>
-                <Card.Title>{title}</Card.Title>
-                <Card.Text>
-                    {details}
-                </Card.Text>
-                <Link to={`/category/${category_id}`}><Button variant="danger">All news in this category</Button></Link>
-            </Card.Body>
-        </Card>
+        <div>
+            <Card className='mb-4'>
+                <Card.Img variant="top" src={image_url} />
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>
+                        {details}
+                    </Card.Text>
+                    <Link to={`/category/${category_id}`}><Button variant="danger">All news in this category</Button></Link>
+                </Card.Body>
+            </Card>
+            <EditorsNews></EditorsNews>
+        </div>
+
     );
 };
 
