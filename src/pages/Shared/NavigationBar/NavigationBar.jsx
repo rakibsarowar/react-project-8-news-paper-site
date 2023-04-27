16cfa04098414a4bc9010eeb63d60f53d884a595
rakibsarowar <rakibsarowar@gmail.com>
2023-04-27 20:55:05 +0600

    working on login page

---
+++ src/pages/Shared/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,43 @@
+import React, { useContext } from 'react';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { FaUserCircle } from 'react-icons/fa';
+import { authContext } from '../../../providers/AuthProvider';
+
+const NavigationBar = () => {
+
+    const { user } = useContext(authContext);
+
+    return (
+        <Container>
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                <Container>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                            <Link to="/category/0">Home</Link>
+                            <Nav.Link href="#pricing">About</Nav.Link>
+                            <Nav.Link href="#pricing">Career</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            {user &&
+
+                                <FaUserCircle style={{ fontSize: '2rem' }}></FaUserCircle>
+                            }
+
+
+                            {user ?
+                                <Button variant="secondary">LogOut</Button> :
+                                <Link to="/login">
+                                </Link>
+                            }
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </Container>
+    );
+};
+
+export default NavigationBar;
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,13 +1,21 @@
 import React, { createContext } from 'react';
+import { getAuth } from "firebase/auth";
+import app from '../firebase/firebase.config';
 
 // Step 01-----------------------------------
 export const authContext = createContext(null);
 
-const AuthProvider = (children) => {
+// Step 04v ---------------------------------------
+const auth = getAuth(app);
+
+const AuthProvider = ({children}) => {
+
+    // step 5 ------------------------------------
+    const user = null
 
     // Step 02 -----------------------------
     const authInfo = {
-        
+        user
     };
 
     // Step 03 ------------------------------- add children
