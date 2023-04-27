cd58546f979a12d7f222d72041826545f942aa8f
rakibsarowar <rakibsarowar@gmail.com>
2023-04-27 15:23:18 +0600

    working on single news rendering

--- src/pages/News/News/News.jsx
+++ src/pages/News/News/News.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const News = () => {
+    const news = useLoaderData();
+    const { _id, title, details, image_url,author,category_id } = news;
+
     return (
-        <div>
-            <h2>News Comming Soon</h2>
-        </div>
+        <Card>
+            <Card.Img variant="top" src={image_url} />
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>
+                    {details}
+                </Card.Text>
+                <Link to={`/category/${category_id}`}><Button variant="danger">All news in this category</Button></Link>
+            </Card.Body>
+        </Card>
     );
 };
 
