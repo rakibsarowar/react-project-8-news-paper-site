3a640bfa57c41ba861f955e1cd5bd76dc4e16f39
rakibsarowar <rakibsarowar@gmail.com>
2023-04-27 14:48:20 +0600

    slicing data and added read more button

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const NewsCard = ({ news }) => {
-    const { _id, title, details } = news;
+    const { _id, title, details, image_url } = news;
     return (
         <Card className="text-center mt-4">
-            <Card.Header>Featured</Card.Header>
+            <Card.Header>{title}</Card.Header>
             <Card.Body>
-                <Card.Title>Special title treatment</Card.Title>
-                <Card.Text>
-                    With supporting text below as a natural lead-in to additional content.
+                <Card.Title>{title}</Card.Title>
+                <Card.Img variant="top" src={image_url} />
+                <Card.Text className='text-start'>
+                    {details.length<250 ? <>{details}</> : <>{details.slice(0,250)}... <Link to={`/news/${_id}`}>Read More</Link></>}
                 </Card.Text>
-                
             </Card.Body>
             <Card.Footer className="text-muted">2 days ago</Card.Footer>
         </Card>
