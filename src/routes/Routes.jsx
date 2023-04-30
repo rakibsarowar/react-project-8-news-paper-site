50892dc8d7c330703b32490636a4e6ce442f45b2
rakibsarowar <rakibsarowar@gmail.com>
2023-04-30 22:27:15 +0600

    completed the modual

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -41,7 +41,7 @@ const router = createBrowserRouter([
             {
                 path: ':id',
                 element:<Category></Category>,
-                loader: ({params}) => fetch(`http://localhost:5000/categories/${params.id}`)
+                loader: ({params}) => fetch(`https://news-paper-site-server-rakibsarowar.vercel.app/categories/${params.id}`)
             }
         ]
     },
@@ -52,7 +52,7 @@ const router = createBrowserRouter([
             {
                 path: ':id',
                 element: <PrivateRoute><News></News></PrivateRoute>,
-                loader: ({params}) => fetch (`http://localhost:5000/news/${params.id}`)
+                loader: ({params}) => fetch (`https://news-paper-site-server-rakibsarowar.vercel.app/news/${params.id}`)
             }
         ]
     }
